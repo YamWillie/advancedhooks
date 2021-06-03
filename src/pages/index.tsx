6e8463439165527eaf6bffe05f0999ee24ac2f90
Willie Yam <willieyam7@gmail.com>
2021-06-03 11:39:48 -0400

    section 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,116 @@
 import React from "react"
-import { Link } from "gatsby"
-import Layout from "../components/layout/layout"
+import styled from "styled-components"
+
 import SEO from "../components/layout/seo"
+import PurchaseButton from "../components/buttons/PurchaseButton"
+import CourseCard from "../components/cards/CourseCard"
 
 function IndexPage() {
   return (
-    <Layout>
+    <Wrapper>
       <SEO title="Home" />
-      <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-    </Layout>
+      <HeroWrapper>
+        <CourseCard />
+        <TextWrapper>
+          <Logo src="/images/logos/react-logo.svg" alt="logo" />
+          <Title>Build a SwiftUI app for iOS 14</Title>
+          <Caption>20 sections - 3 hours of video</Caption>
+          <Description>
+            Build a multi-platform app for iOS, iPadOS and Big Sur
+          </Description>
+          <AuthorWrapper>
+            <AvatarImage src="/images/avatars/Meng.png" alt="avatar" />
+            <Caption>Taught by Meng To</Caption>
+          </AuthorWrapper>
+          <PurchaseButton />
+          <SmallText>
+            Purchase includes access to 30 courses. Over 80 hours of content,
+            including 12 hours for SwiftUI, for iOS 13 and iOS 14.
+          </SmallText>
+        </TextWrapper>
+      </HeroWrapper>
+    </Wrapper>
   )
 }
 
 export default IndexPage
+
+const Wrapper = styled.div`
+  background: linear-gradient(200.44deg, #4316db 13.57%, #9076e7 98.38%);
+`
+
+const HeroWrapper = styled.div`
+  display: grid;
+  max-width: 1234px;
+  grid-template-columns: 360px auto;
+  gap: 60px;
+  padding: 220px 20px 0px;
+  justify-content: center;
+  margin-left: auto;
+  margin-right: auto;
+
+  @media (max-width: 780px) {
+    grid-template-columns: 1fr;
+    justify-items: center;
+  }
+`
+
+const TextWrapper = styled.div`
+  display: grid;
+  gap: 20px;
+
+  @media (max-width: 780px) {
+    justify-items: center;
+    text-align: center;
+  }
+`
+
+const Logo = styled.img`
+  width: 60px;
+  height: 60px;
+`
+
+const Title = styled.h1`
+  max-width: 500px;
+  font-family: SF Pro Display;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 60px;
+  line-height: 72px;
+  color: #ffffff;
+  text-shadow: 0px 20px 40px rgba(0, 0, 0, 0.3);
+`
+
+const Caption = styled.p`
+  font-family: SF Pro Text;
+  font-style: normal;
+  font-size: 15px;
+  color: rgba(255, 255, 255, 0.7);
+  text-transform: uppercase;
+`
+
+const Description = styled.p`
+  font-family: SF Pro Text;
+  font-style: normal;
+  font-size: 20px;
+  color: #ffffff;
+`
+
+const AuthorWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+`
+
+const AvatarImage = styled.img`
+  width: 32px;
+  height: 32px;
+`
+
+const SmallText = styled.p`
+  max-width: 280px;
+  font-family: SF Pro Text;
+  font-size: 13px;
+  line-height: 130%;
+  color: rgba(255, 255, 255, 0.7);
+`
